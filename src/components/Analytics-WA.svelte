9e727aa9e0dabb120936a3de272fca5ea072d870
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-19 19:00:14 +0530

    LaunchFa.st

---
+++ src/components/Analytics-WA.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  /*
+      A component that can be used anywhere in your app
+      Add this to the bottom of the page if you want to use it globally
+  
+      <AnalyticsWa
+        ID="GA-ID-12345678"
+      />
+    */
+
+  import { onMount } from 'svelte'
+
+  export let ID: string
+
+  onMount(() => {
+    if (ID && ID.length > 1) {
+      const waScript = document.createElement('script')
+      waScript.id = 'waa'
+      waScript.defer = true
+      waScript.src = `https://stats.wideangle.co/script/${ID}.js`
+      document.head.appendChild(waScript)
+    }
+  })
+</script>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import SignInWithGoogle from '@/components/Sign-In-With-Google.svelte'
+</script>
+
+<div class="relative mx-auto flex max-w-7xl flex-col px-8 py-8">
+  <SignInWithGoogle />
+</div>
