25c9f7bf3aa0ea52222cb146017794a1349cbeb9
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-31 01:58:28 +0530

    single script

--- write_amplify.mjs
+++ write_amplify.mjs
@@ -1,5 +1,6 @@
 import { join } from 'node:path';
-import { writeFileSync, mkdirSync } from 'node:fs';
+import { nodeFileTrace } from '@vercel/nft';
+import { writeFileSync, mkdirSync, existsSync, cpSync } from 'node:fs';
 
 const deployManifestConfig = {
     version: 1,
@@ -31,7 +32,7 @@ const deployManifestConfig = {
     computeResources: [
         {
             name: "default",
-            entrypoint: "index.js",
+            entrypoint: "build/index.js",
             runtime: "nodejs18.x",
         },
     ],
@@ -41,10 +42,17 @@ const deployManifestConfig = {
     },
 };
 
-mkdirSync(join(process.cwd(), '.amplify-hosting'))
-mkdirSync(join(process.cwd(), '.amplify-hosting', 'static'))
-mkdirSync(join(process.cwd(), '.amplify-hosting', 'compute'))
-mkdirSync(join(process.cwd(), '.amplify-hosting', 'compute', 'default'))
+const amplifyDirectories = [
+    join(process.cwd(), '.amplify-hosting'),
+    join(process.cwd(), '.amplify-hosting', 'static'),
+    join(process.cwd(), '.amplify-hosting', 'compute'),
+    join(process.cwd(), '.amplify-hosting', 'compute', 'default'),
+    join(process.cwd(), '.amplify-hosting', 'compute', 'default', 'node_modules'),
+]
+
+amplifyDirectories.forEach((i => {
+    if (!existsSync(i)) mkdirSync(i)
+}))
 
 const functionsConfigPath = join(
     process.cwd(),
@@ -55,3 +63,40 @@ writeFileSync(
     functionsConfigPath,
     JSON.stringify(deployManifestConfig),
 );
+
+cpSync(join(process.cwd(), 'build', 'client'), amplifyDirectories[1], { recursive: true })
+
+/**
+* Exports a function to write a json that computes dependencies required by given paths
+* @param paths List of paths for which dependencies need to be computed
+* @param filename Name of the file to write json to
+* @returns void
+*/
+async function computeDependencies(paths = []) {
+    // the whole app inside index.js,
+    // include other paths that are
+    // not bundled with your app builds
+    const files = paths
+    // Compute file trace
+    const { fileList } = await nodeFileTrace(files)
+    // Store set of packages
+    let packages = {}
+    fileList.forEach((i) => {
+        if (i.includes('node_modules/')) {
+            let temp = i.replace('node_modules/', '')
+            temp = temp.substring(0, temp.indexOf('/'))
+            packages[`node_modules/${temp}`] = true
+        } else
+            packages[i] = true
+    })
+    // Sort the set of packages by name (for easier difference comparison with git)
+    packages = Object.keys(packages)
+        .sort()
+    // Dump the list of the computed packages for further references while deploying the app
+    packages.forEach(i => {
+        cpSync(i, join(amplifyDirectories[3], i), { recursive: true })
+    })
+
+}
+
+computeDependencies(['./build/index.js'])
