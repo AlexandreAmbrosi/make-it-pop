126f159024a26d24047758011aeec3cb5f63df12
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-06-07 18:10:16 +0530

    dyanmic CSS

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,36 +5,46 @@
     <Header logo="https://rishi.app/static/favicon-image.jpg" />
   */
 
-  export let logo: string = '/purple-icon.png'
+  export let logo: string = 'https://ik.imagekit.io/vjeqenuhn/launchfast-website/purple-icon.png'
 
+  import User from './User.svelte'
   import { slug } from 'github-slugger'
-  import IconCross from 'virtual:icons/mdi/close'
-  import IconHamburger from 'virtual:icons/mdi/hamburger'
-  import ButtonCheckout from './Button-Checkout.svelte'
+  import IconCross from '~icons/mdi/close'
+  import IconHamburger from '~icons/mdi/hamburger'
 
   const header = {
     viewID: slug('header'),
     labelID: 'view-' + slug('header'),
   }
+
+  const css = `#${header['viewID']} {display: none;}#${header['labelID']}:checked + #${header['viewID']} {display: flex;}`
 </script>
 
 <div class="relative mx-auto flex max-w-7xl flex-row items-center justify-between px-8 pt-2">
   <a href="/" class="flex flex-row items-center gap-x-2">
     <img alt="LaunchFast Logo" height="30" width="30" src={logo} class="h-[30px] w-[30px] rounded-full bg-gray-100" loading="lazy" />
-    <span class="text-2xl font-semibold text-launchfast">LaunchFast</span>
+    <span class="text-2xl font-semibold text-branding">LaunchFast</span>
   </a>
   <div class="hidden flex-row items-center gap-x-8 sm:flex">
-    <a class="text-gray-800 hover:text-launchfast hover:underline" href="/#pricing">Pricing</a>
-    <a class="text-gray-800 hover:text-launchfast hover:underline" href="/#demo">Demo</a>
-    <ButtonCheckout minimal={true} />
+    <a class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="/#pricing"> Pricing </a>
+    <a target="_blank" class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="https://launchfast-nextjs-ts.vercel.app"> Demo </a>
+    <a class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="/blog"> Blogs </a>
+    <a
+      class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline"
+      href="https://affiliates.lemonsqueezy.com/programs/code-templates"
+      target="_blank"
+    >
+      Affiliates (40%)
+    </a>
   </div>
-  <label class="rounded-full border p-2 sm:hidden" for={header.labelID}>
+  <User />
+  <label class="sm:hidden" for={header.labelID}>
     <IconHamburger />
   </label>
   <input class="hidden" type="checkbox" id={header.labelID} />
   <div
     id={header.viewID}
-    class="absolute right-0 top-0 z-20 flex h-screen w-[250px] flex-col overflow-hidden border-l bg-white shadow-2xl transition-all duration-300 ease-in-out sm:!hidden"
+    class="fixed right-0 top-0 z-20 flex h-screen w-[250px] flex-col overflow-hidden border-l bg-white shadow-2xl transition-all duration-300 ease-in-out sm:!hidden"
   >
     <div class="flex flex-row items-center justify-between border-b px-5 py-2">
       <span>Menu</span>
@@ -43,10 +53,14 @@
       </label>
     </div>
     <div class="flex flex-col gap-y-4 p-5">
-      <a href="/" class="text-xl font-semibold text-launchfast">Home</a>
-      <a href="/#pricing" class="text-xl">Pricing</a>
-      <a href="/#demo" class="text-xl">Demo</a>
-      <ButtonCheckout minimal={true} />
+      <a target="_blank" class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="https://launchfast-nextjs-ts.vercel.app"> Demo </a>
+      <a class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="/blog"> Blogs </a>
+      <a class="flex flex-row items-center gap-x-2 text-gray-800 hover:text-launchfast hover:underline" href="https://affiliates.lemonsqueezy.com/programs/code-templates">
+        Affiliates (40%)
+      </a>
+      <a href="/#pricing" class="rounded bg-branding px-2 py-1 text-center text-white transition duration-200 hover:-rotate-1 hover:scale-105"> Get LaunchFast &rarr; </a>
     </div>
   </div>
 </div>
+
+{@html `\<style\>${css}\<\/style\>`}
